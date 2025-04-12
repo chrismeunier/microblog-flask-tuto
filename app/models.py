489043c0bc669ef7814af481331ddda07aaaff50
chrismeunier <ch.chrismuller@pm.me>
2025-04-12 07:55:42 +0200

    End Chapter 4

--- app/models.py
+++ app/models.py
@@ -26,3 +26,6 @@ class Post(db.Model):
     user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id), index=True)
 
     author: so.Mapped[User] = so.relationship(back_populates="posts")
+
+    def __repr__(self):
+        return "<Post: {}>".format(self.body)
--- microblog.py
+++ microblog.py
@@ -1,3 +1,13 @@
 from app import app
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+
 # Start the server with `flask run` if `set FLASK_APP=microblog.py`
 # or with `flask --app microblog run`
+
+
+# To start an interpreter session with the app context:
+# `flask shell`
+@app.shell_context_processor
+def make_shell_context():
+    return {"sa": sa, "so": so}
