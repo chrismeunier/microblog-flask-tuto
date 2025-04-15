d29175988e2f1095e374d3231b9f6c0a77c07b60
chrismeunier <ch.chrismuller@pm.me>
2025-04-15 12:12:31 +0200

    C7: added logger to log file

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from pathlib import Path
-
+import logging
+from logging.handlers import RotatingFileHandler
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -16,5 +17,18 @@ login.login_view = "login"
 if not app.debug:
     if not Path("app", "logs").exists():
         Path("app", "logs").mkdir()
+    file_handler = RotatingFileHandler(
+        "logs/microblog.log", maxBytes=10240, backupCount=10
+    )
+    file_handler.setFormatter(
+        logging.Formatter(
+            "%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]"
+        )
+    )
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info("Microblog startup")
 
 from app import routes, models, errors
