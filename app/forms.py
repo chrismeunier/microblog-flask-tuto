9cb3cd189a54f3526e6a34d61e12de68845603d4
chrismeunier <ch.chrismuller@pm.me>
2025-04-18 22:03:08 +0200

    Error to "mail" in terminal from Chapter 7 done

--- app/forms.py
+++ app/forms.py
@@ -45,12 +45,12 @@ class EditProfileForm(FlaskForm):
         super().__init__(*args, **kwargs)
         self.original_username = original_username
 
-    def validate_username(self, username):
-        if username.data == self.original_username:
-            return
-        user = db.session.scalar(sa.select(User).where(User.username == username.data))
-        if user is not None:
-            raise ValidationError("Please use a different username.")
+    # def validate_username(self, username):
+        # if username.data == self.original_username:
+            # return
+        # user = db.session.scalar(sa.select(User).where(User.username == username.data))
+        # if user is not None:
+            # raise ValidationError("Please use a different username.")
 
 
 class EmptyForm(FlaskForm):
--- config.py
+++ config.py
@@ -9,3 +9,12 @@ class Config:
         "DATABASE_URL"
     ) or "sqlite:///" + os.path.join(base_dir, "app.db")
     POSTS_PER_PAGE = 10
+    # To get the "emails" sent when there is an error in a terminal run:
+    # aiosmtpd -n -c aiosmtpd.handlers.Debugging -l 127.0.0.1:8025
+    # or with whatever address and port the app uses
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['sysadmin@aiosmtpd.com']
