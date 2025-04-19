e8fe118badc6cc0c6fe91b5fd92651a9f138c4df
chrismeunier <ch.chrismuller@pm.me>
2025-04-19 15:19:52 +0200

    Chapter 13: installed babel

--- config.py
+++ config.py
@@ -18,3 +18,4 @@ class Config:
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     ADMINS = ["noreply.password.reset@aiosmtpd.com", "sysadmin@aiosmtpd.com"]
+    LANGUAGES = ["en", "fr"]
